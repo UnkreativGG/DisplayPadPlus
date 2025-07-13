0ffd805dacf1838affe02f67e880165837d0253d
Unkreativ <139508770+UnkreativGG@users.noreply.github.com>
2025-07-13 19:12:07 +0200

    Update Program.cs
    This does not exist anymore

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -3,9 +3,5 @@ using DisplayPadPlus.DeviceSystem.Visuals;
 using Testing.Layers;
 
 DisplayPadManager.GlobalDefaultBackground = new Background(@"---", true);
-# if true
 DisplayPadManager.AddDevice<Home>();
-#else
-    DisplayPadManager.AddDevice<MC>();
-#endif
 DisplayPadManager.Finish();
